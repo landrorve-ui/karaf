c3cbddceb23783aea3d4b1c5da1f411cff31fc1a
hoan <nvhoan14@gmail.comm>
2026-05-01 09:43:43 +0000

    fix flow

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig, env } from 'prisma/config';
-import { loadLocalEnv } from './libs/common/src/env';
+import dotenv from 'dotenv';
+dotenv.config({
+  path: ['.env', '.env.local', `.env.${process.env.NODE_ENV}`],
+});
 
-loadLocalEnv();
+console.log('DATABASE_URL:',  process.env.DATABASE_URL);
 
 export default defineConfig({
   schema: 'libs/database/prisma/schema.prisma',
